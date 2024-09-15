4aa8da3655d81e33688408ee3f3695d21d2b35e3
Laksh Kumar <73332171+lakshkumar06@users.noreply.github.com>
2024-09-15 08:17:44 -0500

    upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className='flex  justify-between items-center px-[5%] py-[20px] bg-white text-[18px]'>
-        <img src={process.env.PUBLIC_URL + '/logo.png'} className='w-[15%]'></img>
+    <div className='flex  justify-between items-center px-[3%] py-[20px] bg-white text-[18px] z-100 relative'>
+        <img src={process.env.PUBLIC_URL + '/logo.png'} className='w-[8%]'></img>
         <ul className="flex gap-[20px]">
             <li className='flexCol'>About Us</li>
             <li className='py-[6px] px-[10px] bg-primary text-white flexCol btn'><Link to='/Map'>Open Map</Link></li>
--- src/components/WhyitMatters.js
+++ src/components/WhyitMatters.js
@@ -3,10 +3,12 @@ import React from 'react'
 const WhyitMatters = () => {
   return (
     <div className='flex bg-white py-[80px]'>
-      <img src={process.env.PUBLIC_URL + '/gasplant.jpg'} className='w-[50vw]'></img>
+      <img src={process.env.PUBLIC_URL + '/gasplant.jpg'} className='w-[45vw]' data-aos="slide-right" data-aos-offset="400" data-aos-duration="500"></img>
       <div className='px-[5vw] text-black text-left flexCol'>
         <p className='font-bold text-[50px]'>Why it matters?</p>
-        <p className='text-[20px] pt-[1em]'>Methane is 80 times more potent than carbon dioxide when it comes to trapping heat in the atmosphere. Reducing methane emissions is one of the fastest and most effective ways to slow down climate change. Our platform helps industries take action by providing the data and tools they need to monitor, manage, and minimize methane emissions</p>
+        <p className='text-[18px] pt-[1em]'>Methane is a potent greenhouse gas, and tracking its emissions is crucial for safety and environmental compliance. However, many oil and gas facilities, especially in rural areas like West Texas or regions in South Asia, lack the sophisticated sensors required for real-time monitoring. These regions either face infrastructure challenges or operate under lower safety standards, leaving them vulnerable to undetected emissions.
+
+<br/> By leveraging satellite imagery, we eliminate the need for on-site infrastructure. Our solution provides an efficient and cost-effective way for facilities to monitor methane levels without the need for expensive sensor networks, ensuring that safety and environmental standards can be upheld even in the most remote locations.</p>
       </div>
     </div>
   )
