3a15dfe6bcc1c8c9d5e287ec7af869630995387a
Laksh Kumar <73332171+lakshkumar06@users.noreply.github.com>
2024-09-15 09:40:46 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => {
     <div className='flex  justify-between items-center px-[3%] py-[20px] bg-white text-[18px] z-100 relative'>
         <img src={process.env.PUBLIC_URL + '/logo.png'} className='w-[8%]'></img>
         <ul className="flex gap-[20px]">
-            <li className='flexCol'>About Us</li>
             <li className='py-[6px] px-[10px] bg-primary text-white flexCol btn'><Link to='/Map'>Open Map</Link></li>
         </ul>
     </div>
